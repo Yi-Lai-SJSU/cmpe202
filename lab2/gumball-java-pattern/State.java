a6572d2d6ddb2c9132855afd8500cae38cfd9a41
Yi Lai <42703161+Yi-Lai-SJSU@users.noreply.github.com>
2018-09-07 23:40:12 -0700

    Delete State.java

--- lab2/gumball-java-pattern/State.java
+++
@@ -1,7 +0,0 @@
-public interface State {
-	public void insertCoins(int valueOfCoin);
-	public void ejectCoins();
-	public void turnCrank();
-	public void dispense();
-	public void refill(int numberGumballs);
-}
