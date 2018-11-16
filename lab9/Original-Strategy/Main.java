900c45476af2300dcab0ec480d65dadd24503f69
Yi-Lai-SJSU <yi.lai@sjsu.edu>
2018-11-16 13:05:40 -0800

    modify the original Main.java

--- lab9/Original-Strategy/Main.java
+++ lab9/Original-Strategy/Main.java
@@ -1,18 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StrategyTest strategyTest = new StrategyTest("hello");
 
-	StrategyTest strategyTest = new StrategyTest("hello");
+		while(true) {
+			System.out.print("Enter a number(0:quit): ");
+			int number = in.nextInt();
+			if (number == 0) {
+				break;
+			}
 
-	strategyTest.setIstrategy(new ConcreteStrategy1());
-	strategyTest.operate();
-
-	strategyTest.setIstrategy(new ConcreteStrategy2());
-	strategyTest.operate();
-
-	strategyTest.setIstrategy(new ConcreteStrategy3());
-	strategyTest.operate();
+			switch(number%3) {
+				case 0:
+					strategyTest.setIstrategy(new ConcreteStrategy1());
+					strategyTest.operate();
+					break;
+				case 1:
+					strategyTest.setIstrategy(new ConcreteStrategy2());
+					strategyTest.operate();
+					break;
+				case 2:
+					strategyTest.setIstrategy(new ConcreteStrategy3());
+					strategyTest.operate();
+					break;
+			}
+		}
     }
 }
